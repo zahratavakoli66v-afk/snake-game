c67852ca9c8f9f852ef9c3cb78853ccf6f0783dd
zahratavakoli66v-afk <zahratavakoli66v@gmail.com>
2026-08-03 14:41:50 +0430

    add socket io online game

--- server.js
+++ server.js
@@ -1,24 +1,63 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 
 const server = http.createServer(app);
 
+const io = new Server(server);
 
-// تست اصلی
-app.get("/", (req, res) => {
-    res.send("Snake Game Server OK");
-});
-
-
-// نمایش فایل‌های public
 app.use(express.static("public"));
+
+let players = {};
+
+io.on("connection", (socket) => {
+
+    console.log("بازیکن وصل شد:", socket.id);
+
+    socket.on("joinGame", (data) => {
+
+        players[socket.id] = data.name;
+
+        io.emit(
+            "playersList",
+            Object.values(players)
+        );
+
+    });
+
+
+    socket.on("chatMessage", (message) => {
+
+        io.emit("chatMessage", {
+            name: players[socket.id],
+            message: message
+        });
+
+    });
+
+
+    socket.on("disconnect", () => {
+
+        delete players[socket.id];
+
+        io.emit(
+            "playersList",
+            Object.values(players)
+        );
+
+    });
+
+});
 
 
 const PORT = process.env.PORT || 3000;
 
-
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+
+    console.log(
+        "Server running on port " + PORT
+    );
+
 });
