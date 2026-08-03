2b9adb66c0704bd2309850c4a2b8bf873c0e8bb6
zahratavakoli66v-afk <zahratavakoli66v@gmail.com>
2026-08-03 15:32:24 +0430

    host start button and slower snake

--- server.js
+++ server.js
@@ -1,63 +1,86 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let players = {};
+let hostId = null;
+let gameStarted = false;
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
 
-    console.log("بازیکن وصل شد:", socket.id);
+  socket.on('joinGame', (data) => {
 
-    socket.on("joinGame", (data) => {
+    players[socket.id] = data.name;
 
-        players[socket.id] = data.name;
+    // اولین بازیکن میزبان می‌شود
+    if(!hostId){
+      hostId = socket.id;
+    }
 
-        io.emit(
-            "playersList",
-            Object.values(players)
-        );
+    io.emit('playersList', Object.values(players));
 
+    io.emit('hostInfo', {
+      hostId,
+      gameStarted
     });
 
+  });
 
-    socket.on("chatMessage", (message) => {
+  socket.on('startGame', () => {
 
-        io.emit("chatMessage", {
-            name: players[socket.id],
-            message: message
-        });
+    if(socket.id !== hostId) return;
 
+    gameStarted = true;
+
+    io.emit('gameStarted');
+
+  });
+
+  socket.on('chatMessage', (message) => {
+
+    io.emit('chatMessage', {
+      name: players[socket.id],
+      message
     });
 
+  });
 
-    socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
 
-        delete players[socket.id];
+    delete players[socket.id];
 
-        io.emit(
-            "playersList",
-            Object.values(players)
-        );
+    // اگر میزبان خارج شد
+    if(socket.id === hostId){
 
+      const ids = Object.keys(players);
+
+      hostId = ids.length ? ids[0] : null;
+
+      gameStarted = false;
+
+    }
+
+    io.emit('playersList', Object.values(players));
+
+    io.emit('hostInfo', {
+      hostId,
+      gameStarted
     });
+
+  });
 
 });
-
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-
-    console.log(
-        "Server running on port " + PORT
-    );
-
+  console.log('Server running on port ' + PORT);
 });
+```
