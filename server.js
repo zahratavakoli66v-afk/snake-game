5568b46c784689b25f7709de9b551035eb99bfd1
zahratavakoli66v-afk <zahratavakoli66v@gmail.com>
2026-08-03 12:07:47 +0430

    first version

---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+
+// فایل‌های پوشه public را نمایش بده
+app.use(express.static("public"));
+
+
+// لیست بازیکنان
+let players = {};
+
+
+
+// وقتی بازیکن وصل شد
+io.on("connection", (socket) => {
+
+    console.log("یک بازیکن وصل شد:", socket.id);
+
+
+
+    // ورود بازیکن
+    socket.on("joinGame", (data) => {
+
+        players[socket.id] = data.name;
+
+
+        console.log(
+            "بازیکن وارد شد:",
+            data.name
+        );
+
+
+        // ارسال لیست بازیکنان به همه
+        io.emit(
+            "playersList",
+            Object.values(players)
+        );
+
+    });
+
+
+
+    // دریافت پیام چت
+    socket.on("chatMessage", (message) => {
+
+
+        const name = players[socket.id];
+
+
+        io.emit("chatMessage", {
+
+            name: name,
+
+            message: message
+
+        });
+
+
+    });
+
+
+
+    // وقتی بازیکن خارج شد
+    socket.on("disconnect", () => {
+
+
+        console.log(
+            "بازیکن خارج شد:",
+            players[socket.id]
+        );
+
+
+        delete players[socket.id];
+
+
+        // بروزرسانی لیست
+        io.emit(
+            "playersList",
+            Object.values(players)
+        );
+
+
+    });
+
+
+
+});
+
+
+
+// اجرای سرور
+server.listen(3000, () => {
+
+    console.log(
+        "سرور بازی روی پورت 3000 اجرا شد"
+    );
+
+});
